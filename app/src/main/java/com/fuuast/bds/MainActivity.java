7469fd91bc6c5fd304061cf51b9a430ff4f9d932
syammehar <36952618+syammehar@users.noreply.github.com>
2020-02-04 11:03:41 +0500

    initial create

---
+++ app/src/main/java/com/fuuast/bds/MainActivity.java
@@ -0,0 +1,14 @@
+package com.fuuast.bds;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
